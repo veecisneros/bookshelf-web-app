bf3ac7faaa8176cb996fd5c46bca4c269795b860
veronica cisneors <vcisneroshdz@gmail.com>
2023-02-21 13:41:26 -0600

    new front-end changes

--- js/index.js
+++ js/index.js
@@ -38,18 +38,7 @@ return bookTitles
 
 //DOM rendering
 function bookRender(){
-    
-    const library =  document.createElement('h2')
-    library.textContent = "Welcome to Bookifi";
-   
-    
-    const about =  document.createElement("p")
-    about.textContent = "If your a bookworm and love to know more about a book before you pick it up at your local book store or public library this is the place for you! You are welcome to add your own book titles and explore others comments and reviews.";
-    
-    document.body.append(library);
-    document.body.append(about);
-
-    //I actually want this list of books in another page-- not the landing page
+    //rendering the books in my Books page
     const booklist = document.createElement('ul');
     booklist.replaceChildren(...listofBooks);
     document.body.append(booklist);

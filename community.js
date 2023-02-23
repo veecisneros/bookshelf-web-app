8b8901cca00697792ea391ecb08d76a1a3eeb535
veronica cisneors <vcisneroshdz@gmail.com>
2023-02-22 21:13:32 -0600

    user add books

---
+++ community.js
@@ -0,0 +1,16 @@
+// js code for users to add new books
+
+// let addingBook = new Books
+// let addBookBtn = document.getElementById('add-btn')
+
+// addBookBtn.addEventListener('click', ()=>{
+//     let title = document.getElementById('title').value;
+//     let author = document.getElementById('author').value;
+//     let subject = document.getElementById('subject').value;
+//     let language = document.getElementById('language').value;
+
+//     let newBooks = new Books(title, author, subject, language)
+
+//     addingBook.addbook(newBooks) 
+
+// });

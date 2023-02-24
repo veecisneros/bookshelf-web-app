270a7226b0172eb6a9e34af6bdd849a233e142c2
veronica cisneors <vcisneroshdz@gmail.com>
2023-02-24 10:38:51 -0600

    working on filter

--- js/index.js
+++ js/index.js
@@ -18,18 +18,28 @@ class Books{
         this.language = language
     }
 }
-
 //checking to see if adding books works
 // let newBook = new Books('cinderella','disney','fairytales', 'english');
 // console.log(newBook)
 
+//filter function to filter by language:
+
+// const lookForEn = document.getElementById('en-btn')
+// const lookForDe = document.getElementById('de-btn')
+// const lookForFr = document.getElementById('fr-btn')
+// const lookForFi = document.getElementById('fi-btn')
+
+// const results = booklist.filter((e)=>{
+//     return applyFilter(e,fileters)
+// });
+
+
 // users add new books here
 const newBookList = document.createElement('ul');
 const myBookshelf =  new Bookshelf();
+
 function addNewBookRender(){
-
 const addBookBtn = document.getElementById('addbookbtn');
-
 addBookBtn.addEventListener('click', () =>{
 let title = document.getElementById('title').value;
 let author = document.getElementById('author').value;
@@ -45,6 +55,7 @@ newAddedBook.textContent = `${newBook.title} writen by ${newBook.author}`;
 newBookList.appendChild(newAddedBook)
 document.body.append(newBookList);
 });
+
 }
 addNewBookRender()
 
@@ -58,41 +69,61 @@ let listofBooks = bookData.map((book) => {
 
 // buttons for more info on book when pressed
 const moreInfoButton = document.createElement('button');
+moreInfoButton.setAttribute('id','moreinfo-btn');
 moreInfoButton.textContent = 'more info';
 moreInfoButton.addEventListener('click', ()=> {
     const additionalInfo = document.createElement('p');
     additionalInfo.textContent = `${book.subject}, ${book.language}`;
     bookTitles.appendChild(additionalInfo);
-})
+});
 bookTitles.appendChild(moreInfoButton);
 
 
 //like button display w/ event listener
 const likeButton = document.createElement('button');
+likeButton.setAttribute('id','like-btn');
 likeButton.textContent = "❤";
+//add event listner if i have time
 bookTitles.appendChild(likeButton);
 
 // comment section here
 // needs a boolean w/280 limit
 // needs and enter button or an enter function in the addeventlistner
 const commentButton = document.createElement('button');
+commentButton.setAttribute('id', 'comment-btn');
 commentButton.textContent = 'comments/reviews';
-commentButton.addEventListener('click',()=>{
+commentButton.addEventListener ('click',()=>{
     const addCommentsHere = document.createElement('textarea');
-    addCommentsHere.textContent = '...';
-    addCommentsHere.addEventListener('mouseenter',()=>{
-        const clickEnter = document.createElement('p').value;
-        clickEnter.textContent = p.value;
-    })
+    addCommentsHere.textContent = "...";
+
+     addCommentsHere.setAttribute('maxlength', '280');
+
+     const submitButton = document.createElement('button');
+    submitButton.textContent = "submit";
+    submitButton.setAttribute('id','submit-btn')
+    submitButton.addEventListener ('click',()=>{
+        const clickToSubmit = document.createElement('p');
+        clickToSubmit.textContent = addCommentsHere.value;
+        bookTitles.append(clickToSubmit);
+    });
+    bookTitles.appendChild(submitButton);
+
+       addCommentsHere.addEventListener('input',()=>{
+        if (addCommentsHere.value.length <= 280){
+            submitButton.disabled = false;
+        }else {
+            submitButton.disabled = true;
+        }
+    });
+    
+
     bookTitles.appendChild(addCommentsHere);
-    bookTitles.appendChild(clickEnter);
-});
-bookTitles.appendChild(commentButton);
-return bookTitles;
-});
-
-
-
+       });
+       bookTitles.appendChild(commentButton);
+       return bookTitles;
+    });
+  
+   
 function bookRender(){
 //rendering the books in my Books page
 //spread operator to seperate list from Ul
@@ -102,4 +133,3 @@ function bookRender(){
 }
 
 bookRender()
-
